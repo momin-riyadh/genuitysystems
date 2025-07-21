d2c2535175c1319c1e8eb7b122fd03252348f5c8
momin-riyadh <mominriyadh@gmail.com>
2025-07-21 11:43:17 +0600

    fix and deploy blogs

--- blogs.php
+++ blogs.php
@@ -26,7 +26,7 @@
                         <h3><a href="blog-post.php">Sample Blog Post Title 1</a></h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt nunc lorem, nec
                             faucibus mi facilisis eget.</p>
-                        <a href="blog-post.php" class="btn btn-sm btn-primary">Read More</a>
+                        <a href="blog-post.php" class="g-blog-btn">Read More</a>
                     </div>
                 </div>
             </div>
@@ -43,7 +43,7 @@
                         <h3><a href="blog-post.php">Sample Blog Post Title 2</a></h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt nunc lorem, nec
                             faucibus mi facilisis eget.</p>
-                        <a href="blog-post.php" class="btn btn-primary">Read More</a>
+                        <a href="blog-post.php" class="g-blog-btn">Read More</a>
                     </div>
                 </div>
             </div>
@@ -60,7 +60,7 @@
                         <h3><a href="blog-post.php">Sample Blog Post Title 3</a></h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt nunc lorem, nec
                             faucibus mi facilisis eget.</p>
-                        <a href="blog-post.php" class="btn btn-primary">Read More</a>
+                        <a href="blog-post.php" class="g-blog-btn">Read More</a>
                     </div>
                 </div>
             </div>
@@ -76,7 +76,7 @@
                         <h3><a href="blog-post.php">Sample Blog Post Title 3</a></h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt nunc lorem, nec
                             faucibus mi facilisis eget.</p>
-                        <a href="blog-post.php" class="btn btn-primary">Read More</a>
+                        <a href="blog-post.php" class="g-blog-btn">Read More</a>
                     </div>
                 </div>
             </div>
@@ -92,7 +92,7 @@
                         <h3><a href="blog-post.php">Sample Blog Post Title 3</a></h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt nunc lorem, nec
                             faucibus mi facilisis eget.</p>
-                        <a href="blog-post.php" class="btn btn-primary">Read More</a>
+                        <a href="blog-post.php" class="g-blog-btn">Read More</a>
                     </div>
                 </div>
             </div>
@@ -108,7 +108,7 @@
                         <h3><a href="blog-post.php">Sample Blog Post Title 3</a></h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt nunc lorem, nec
                             faucibus mi facilisis eget.</p>
-                        <a href="blog-post.php" class="btn btn-primary">Read More</a>
+                        <a href="blog-post.php" class="g-blog-btn">Read More</a>
                     </div>
                 </div>
             </div>
@@ -118,4 +118,48 @@
     </div>
 </section>
 
+<!-- Pagination Component -->
+<div class="pagination-container container">
+    <div class="row">
+        <div class="col-md-12">
+            <nav aria-label="Blog pagination">
+                <ul class="custom-pagination">
+                    <!-- Previous Button -->
+                    <li class="page-item disabled">
+                <span class="page-link prev-next">
+                    <i class="fa fa-chevron-left"></i>
+                    <span class="sr-only">Previous</span>
+                </span>
+                    </li>
+
+                    <!-- Page Numbers -->
+                    <li class="page-item active">
+                        <a class="page-link" href="#" data-page="1">1</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" data-page="2">2</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" data-page="3">3</a>
+                    </li>
+                    <li class="page-item">
+                        <span class="page-link ellipsis">...</span>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" data-page="10">10</a>
+                    </li>
+
+                    <!-- Next Button -->
+                    <li class="page-item">
+                        <a class="page-link prev-next" href="#" data-page="2">
+                            <i class="fa fa-chevron-right"></i>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+</div>
 <?php include('footer.php') ?>
