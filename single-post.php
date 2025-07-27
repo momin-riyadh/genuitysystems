ab73a0c1f0f0a6284bfa1827c1c51b83e8eb6b28
momin-riyadh <mominriyadh@gmail.com>
2025-07-27 11:57:52 +0600

    fix and deploy blogs

---
+++ single-post.php
@@ -0,0 +1,64 @@
+<?php include('header.php') ?>
+    <div class="container g-single-post-area">
+        <div class="row">
+            <!-- Main Content -->
+            <div class="col-md-8">
+                <article class="blog-post-single">
+                    <h1 class="post-title">The Future of Artificial Intelligence in Business</h1>
+                    <div class="post-meta">
+                        <span class="post-date">July 27, 2025</span>
+                        <span class="post-author">by John Doe</span>
+                    </div>
+                    <div class="post-image mt-4">
+                        <img src="https://picsum.photos/800/400?random" alt="Blog Post Image" class="img-responsive">
+                    </div>
+                    <div class="post-content mt-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                        <h2>The Impact of AI on Modern Business</h2>
+                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                            mollit anim id est laborum.</p>
+
+                        <h2>Key Considerations for Implementation</h2>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                            architecto beatae vitae dicta sunt explicabo.</p>
+                    </div>
+                </article>
+            </div>
+
+            <!-- Sidebar -->
+            <div class="col-md-4">
+                <div class="g-sidebar-right">
+                    <h3 class="g-sidebar-title">Recent Posts</h3>
+                    <div class="g-recent-posts">
+                        <div class="g-recent-post-item">
+                            <img src="https://picsum.photos/100/100?random=1" alt="Recent Post" class="g-recent-post-img">
+                            <div class="g-recent-post-info">
+                                <h4><a href="#">Machine Learning Trends in 2025</a></h4>
+                                <span class="g-post-date">July 25, 2025</span>
+                            </div>
+                        </div>
+                        <div class="g-recent-post-item">
+                            <img src="https://picsum.photos/100/100?random=2" alt="Recent Post" class="g-recent-post-img">
+                            <div class="g-recent-post-info">
+                                <h4><a href="#">Digital Transformation Success Stories</a></h4>
+                                <span class="g-post-date">July 23, 2025</span>
+                            </div>
+                        </div>
+                        <div class="g-recent-post-item">
+                            <img src="https://picsum.photos/100/100?random=3" alt="Recent Post" class="g-recent-post-img">
+                            <div class="g-recent-post-info">
+                                <h4><a href="#">Cloud Computing Best Practices</a></h4>
+                                <span class="g-post-date">July 20, 2025</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include('footer.php') ?>
